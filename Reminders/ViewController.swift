f2cc866415d867a84908d66b5a46bb3d21a6f96d
Cal Stephens <cal@calstephens.tech>
2018-02-11 22:55:42 -0500

    Initial Commit

---
+++ Reminders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by Cal Stephens on 2/11/18.
+//  Copyright © 2018 GT iOS Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
